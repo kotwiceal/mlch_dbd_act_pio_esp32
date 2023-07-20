fcb7f511d4c841094328c9649a34d5433267267a
kotwiceal <kotvitskyaya@gmail.com>
2023-07-20 13:53:04 +0300

    Add functional of Wi-Fi access point

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // include libraries 
 #include <devices.h>
+#include <wifi_loc.h>
 
 // conditional compilation
 #define SERIAL_PORT // use serial port;
@@ -10,6 +11,8 @@ void setup() {
 	#ifdef SERIAL_PORT
 		Serial.begin(115200);
 	#endif
+
+	initiate_wifi(&WiFi, &wifi_configuration);
 
     initiate_ltc2636();
 	initiate_si5351();
